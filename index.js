759409b72cfa05194d5a63f8e69243316b3c445e
catherineotondi <catherineotondi@gmail.com>
2022-04-06 17:48:55 +0300

    added random and modulus and maximum computations in the index.js file

--- index.js
+++ index.js
@@ -9,6 +9,19 @@
     let random=Math.floor(Math.random() * 10);
 }
 
+{
+    let num3 = Number(44);
+    let num4 =Number(10);
+    let mod= num3 % num4;
+    
+}
+
+{
+    let max = Math.max(2, 3, 20, 8);
+    
+}
+
+
 
 
 
