0086ea87fd21b5b61b0a4f0a7cf04037252f276c
catherineotondi <catherineotondi@gmail.com>
2022-04-06 17:22:33 +0300

    added multiplication and random variables computations in the index.js file

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+{
+    let num1 = Number(31);
+    let num2 =Number(2);
+    let multiply = num1 * num2;
+    
+}
 
-// Write your code here
+{
+    let random=Math.floor(Math.random() * 10);
+}
+
+
+
+
